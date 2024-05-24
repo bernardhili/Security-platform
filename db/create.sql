876cce0f32ebf22ea858f2aa275518d33846fb85
Bernard.Hili <bernardhili@protonmail.com>
2024-05-24 21:58:05 +0200

    added docker compose file.

--- db/create.sql
+++ db/create.sql
@@ -1,17 +1,22 @@
+CREATE USER compliance;
 CREATE DATABASE compliance;
+GRANT ALL PRIVILEGES ON DATABASE compliance TO compliance;
 
-CREATE TABLE framework (
-    frameworkId INTEGER,
-    title TEXT
+\c compliance;
+CREATE SCHEMA security_compliance;
+
+CREATE TABLE IF NOT EXISTS security_compliance.framework (
+    frameworkId SERIAL PRIMARY KEY,
+    title VARCHAR(250)
 );
 
-CREATE TABLE Control (
-    controlId INTEGER,
-    title TEXT,
-    description TEXT,
-    evidence TEXT,
-    frequency TEXT,
+CREATE TABLE IF NOT EXISTS security_compliance.control (
+    controlId SERIAL PRIMARY KEY,
+    title VARCHAR(250),
+    description VARCHAR(250),
+    evidence VARCHAR(250),
+    frequency VARCHAR(250),
     CS INTEGER,
-    coverage TEXT
+    coverage VARCHAR(250)
 
 );
